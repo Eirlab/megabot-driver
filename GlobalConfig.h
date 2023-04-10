e492e1794ae21ab3bf27c16956e5da192b1257ab
leov66 <leo.v66.lov@gmail.com>
2023-04-10 10:22:48 +0200

    feat(driver): add PinOut and GlobalConfig

---
+++ GlobalConfig.h
@@ -0,0 +1,32 @@
+#ifndef MEGABOT_DRIVER_GLOBALCONFIG_H
+#define MEGABOT_DRIVER_GLOBALCONFIG_H
+
+#define BaudRateNano            115200
+
+#define PeriodUsController      40          // 25 kHz
+
+#define RangeLinearActuator     200         // 20 cm, 200 mm
+
+#define event_TimeOut_Tx        0x10
+
+#define LegA_baseLeg_Pos_Min    0
+#define LegA_baseLeg_Pos_Max    1023
+
+#define LegA_middleLeg_Pos_Min  0
+#define LegA_middleLeg_Pos_Max  1023
+
+#define LegA_endLeg_Pos_Min      0
+#define LegA_endLeg_Pos_Max     1023
+
+#define LegB_baseLeg_Pos_Min    0
+#define LegB_baseLeg_Pos_Max    1023
+
+#define LegB_middleLeg_Pos_Min  0
+#define LegB_middleLeg_Pos_Max  1023
+
+#define LegB_endLeg_Pos_Min     0
+#define LegB_endLeg_Pos_Max     1023
+
+
+
+#endif //MEGABOT_DRIVER_GLOBALCONFIG_H
--- PinOut.h
+++ PinOut.h
@@ -2,49 +2,40 @@
 #define MEGABOT_DRIVER_PINOUT_H
 #include "mbed.h"
 
-#define Mux_PinOut_A        PB_1
-#define Mux_PinOut_B        PB_2
-#define Mux_PinOut_TX_LEG1  PA_10
-#define Mux_PinOut_TX_LEG2  PC_5
+#define pin_Mux_PinOut_A        PB_1
+#define pin_Mux_PinOut_B        PB_2
+#define pin_Mux_PinOut_TX_LEGA  PA_10
+#define pin_Mux_PinOut_TX_LEGB  PC_5
 
-DigitalOut(Mux_PinOut_A);
-DigitalOut(Mux_PinOut_B);
+#define pin_LegA_baseLeg_PWM            PB_7
+#define pin_LegA_baseLeg_DIR1           PC_14
+#define pin_LegA_baseLeg_DIR2           PC_15
+#define pin_LegA_baseLeg_trigMinMax     PH_0
 
-#define Leg1_baseLeg_PWM            PB_7
-#define Leg1_baseLeg_DIR1           PC_14
-#define Leg1_baseLeg_DIR2           PC_15
-#define Leg1_baseLeg_trigMinMax     PH_0
+#define pin_LegA_middleLeg_PWM          PB_0
+#define pin_LegA_middleLeg_DIR1         PC_2
+#define pin_LegA_middleLeg_DIR2         PC_3
+#define pin_LegA_middleLeg_trigMinMax   PC_1
 
-#define Leg1_middleLeg_PWM          PB_0
-#define Leg1_middleLeg_DIR1         PC_2
-#define Leg1_middleLeg_DIR2         PC_3
-#define Leg1_middleLeg_trigMinMax   PC_1
+#define pin_LegA_endLeg_PWM             PA_15
+#define pin_LegA_endLeg_DIR1            PA_14
+#define pin_LegA_endLeg_DIR2            PA_13
+#define pin_LegA_endLeg_trigMinMax      PC_12
 
-#define Leg1_endLeg_PWM             PA_15
-#define Leg1_endLeg_DIR1            PA_14
-#define Leg1_endLeg_DIR2            PA_13
-#define Leg1_endLeg_trigMinMax      PC_12
+#define pin_LegB_baseLeg_PWM            PC_9
+#define pin_LegB_baseLeg_DIR1           PC_8
+#define pin_LegB_baseLeg_DIR2           PC_6
+#define pin_LegB_baseLeg_trigMinMax     PB_8
 
-#define Leg2_baseLeg_PWM            PC_9
-#define Leg2_baseLeg_DIR1           PC_8
-#define Leg2_baseLeg_DIR2           PC_6
-#define Leg2_baseLeg_trigMinMax     PB_8
+#define pin_LegB_middleLeg_PWM          PB_4
+#define pin_LegB_middleLeg_DIR1         PB_5
+#define pin_LegB_middleLeg_DIR2         PB_3
+#define pin_LegB_middleLeg_trigMinMax   PB_10
 
-#define Leg2_middleLeg_PWM          PB_4
-#define Leg2_middleLeg_DIR1         PB_5
-#define Leg2_middleLeg_DIR2         PB_3
-#define Leg2_middleLeg_trigMinMax   PB_10
-
-
-#define Leg2_endLeg_PWM             PB_15
-#define Leg2_endLeg_DIR1            PB_14
-#define Leg2_endLeg_DIR2            PB_13
-#define Leg2_endLeg_trigMinMax      PB_12
-
-
-
-
-
+#define pin_LegB_endLeg_PWM             PB_15
+#define pin_LegB_endLeg_DIR1            PB_14
+#define pin_LegB_endLeg_DIR2            PB_13
+#define pin_LegBb_endLeg_trigMinMax      PB_12
 
 
 struct PinOut_LinearActuator_t {
