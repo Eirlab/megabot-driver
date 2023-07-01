7e4dcfa332d19dd5fdb5758ce3b0720c59c200cf
leov66 <leo.v66.lov@gmail.com>
2023-07-01 17:53:04 +0200

    Ajout de run

--- Mux/MuxCommunication.cpp
+++ Mux/MuxCommunication.cpp
@@ -1,10 +1,11 @@
 #include "MuxCommunication.h"
-<<<<<<< Updated upstream
-#include "MasterCommunication.h"
+
+DigitalOut led(LED1);
 
 MuxCommunication::MuxCommunication(PinName channelA, PinName channelB, PinName TX_legA, PinName TX_legB,
                                    EventFlags *mainFlags,
-                                   Leg *leg_A, Leg *leg_B) {
+                                   Leg *leg_A, Leg *leg_B)
+{
     A = new DigitalOut(channelA);
     B = new DigitalOut(channelB);
     TXLegA = new BufferedSerial(NC, TX_legA, BaudRateNano);
@@ -15,142 +16,119 @@ MuxCommunication::MuxCommunication(PinName channelA, PinName channelB, PinName T
 
     flag = mainFlags;
 
-
-
     linearActuatorSelected = baseLeg;
     legSelected = legA->getId();
     vA = 0;
     cA = 0;
     vB = 0;
     cB = 0;
-=======
-
-MuxCommunication::MuxCommunication(PinName channelA, PinName channelB, PinName TX_leg1, PinName TX_leg2,
-                                   EventFlags *emergency, ComLinearActuator *interface_linear_actuator) {
-    A = new DigitalOut(channelA);
-    B = new DigitalOut(channelB);
-    TXLeg1 = new BufferedSerial(TX_leg1, NC, BaudRate);
-    TXLeg2 = new BufferedSerial(TX_leg2, NC, BaudRate);
-
-    interfaceLA = interface_linear_actuator;
-
-    emergencyFlag = emergency;
-
->>>>>>> Stashed changes
     wordAB = 0;
 }
 
+void MuxCommunication::run(){
+    MuxThread.start(callback(this, &MuxCommunication::threadWorker));
+}
 
-void MuxCommunication::threadWorker() {
-<<<<<<< Updated upstream
-    if (TXLegA->readable() && TXLegB->readable()) {
-        flag->set(event_TimeOut_Tx);
+void MuxCommunication::threadWorker()
+{
+    led = 1;
+    while (1)
+    {
+        if (TXLegA->readable() && TXLegB->readable())
+        {
+            flag->set(1 << 7);
 
-        uint16_t mesureInt;
+            uint16_t mesureInt;
 
-        // Read TXLegA
-        TXLegA->read(&cA, sizeof(cA));
+            // Read TXLegA
+            TXLegA->read(&cA, sizeof(cA));
+            vA = (vA >> 8) | (cA << 24);
+            if (check_nano(vA, mesureInt))
+            {
+                legA->updateValuePositionInt(linearActuatorSelected, mesureInt);
+            }
 
-        vA = (vA >> 8) | (cA << 24);
+            // Read TXLegB
+            TXLegB->read(&cB, sizeof(cB));
+            vB = (vB >> 8) | (cB << 24);
+            if (check_nano(vB, mesureInt))
+            {
+                legB->updateValuePositionInt(linearActuatorSelected, mesureInt);
+            }
 
-        if (check_nano(vA, mesureInt)) {
-            legA->updateValuePositionInt(linearActuatorSelected, mesureInt);
-        }
-
-
-        // Read TXLegB
-        TXLegB->read(&cB, sizeof(cB));
-        vB = (vB >> 8) | (cB << 24);
-        if (check_nano(vB, mesureInt)) {
-            legB->updateValuePositionInt(linearActuatorSelected, mesureInt);
-=======
-    if (TXLeg1->readable() && TXLeg2->readable()) {
-        std::string str;
-        char c1, c2;
-        uint32_t v1, v2;
-        uint32_t mesureInt;
-
-        // Read TXLeg1
-        TXLeg1->read(&c1, sizeof(c1));
-
-        v1 = (v1 >> 8) | (c1 << 24);
-
-        if (check_nano(v1, legSelected, linearActuatorSelected, mesureInt)) {
-            interfaceLA->updateValue(legSelected, linearActuatorSelected, mesureInt);
-        }
-
-
-        // Read TXLeg2
-        TXLeg2->read(&c2, sizeof(c2));
-        v2 = (v2 >> 8) | (c2 << 24);
-        if (check_nano(v2, legSelected, linearActuatorSelected, mesureInt)) {
-            interfaceLA->updateValue(legSelected, linearActuatorSelected, mesureInt);
->>>>>>> Stashed changes
-        }
-
-
-        switch (wordAB) {
+            switch (wordAB)
+            {
             case 0:
                 A->write(0);
                 B->write(1);
+                linearActuatorSelected = middleLeg;
                 break;
             case 1:
                 A->write(1);
                 B->write(0);
+                linearActuatorSelected = endLeg;
                 break;
             case 2:
                 A->write(0);
                 B->write(0);
+                linearActuatorSelected = baseLeg;
                 break;
             default:
                 A->write(0);
                 B->write(0);
-        }
+            }
 
-        wordAB = A->read() << 1 | B->read();
+            wordAB = A->read() << 1 | B->read();
+        }
     }
 }
 
-
-<<<<<<< Updated upstream
-bool MuxCommunication::check_nano(uint32_t value, uint16_t &mesureInt) {
-    struct nanoValue *v = (struct nanoValue *) &value;
-    if (v->header == 0x55) {
-        if (v->check == get_crc(value & 0xFF, (value >> 8) & 0xFF)) {
+bool MuxCommunication::check_nano(uint32_t value, uint16_t &mesureInt)
+{
+    struct nanoValue *v = (struct nanoValue *)&value;
+    if (v->header == 0x55)
+    {
+        if (v->check == get_crc(value & 0xFF, (value >> 8) & 0xFF))
+        {
             // Vérification de la Leg
-            switch (v->legId) {
-                case 1:
-                    legSelected = leg1;
-                    break;
-                case 2:
-                    legSelected = leg2;
-                    break;
-                case 3:
-                    legSelected = leg3;
-                    break;
-                case 4:
-                    legSelected = leg4;
-                    break;
-                default:
-                    return false;
+            switch (v->legId)
+            {
+            case 1:
+                legSelected = leg1;
+                break;
+            case 2:
+                legSelected = leg2;
+                break;
+            case 3:
+                legSelected = leg3;
+                break;
+            case 4:
+                legSelected = leg4;
+                break;
+            default:
+                return false;
             }
             // Vérification de la LinearActuator
-            switch (v->actuatorId) {
-                case 1:
-                    linearActuatorSelected = baseLeg;
-                    break;
-                case 2:
-                    linearActuatorSelected = middleLeg;
-                    break;
-                case 3:
-                    linearActuatorSelected = endLeg;
-                    break;
-                default:
-                    return false;
-            };
-            if (v->value > 1023 || v->value < 0) {
+            switch (v->actuatorId)
+            {
+            case 1:
+                linearActuatorSelected = baseLeg;
+                break;
+            case 2:
+                linearActuatorSelected = middleLeg;
+                break;
+            case 3:
+                linearActuatorSelected = endLeg;
+                break;
+            default:
                 return false;
-            } else {
+            };
+            if (v->value > 1023 || v->value < 0)
+            {
+                return false;
+            }
+            else
+            {
                 mesureInt = v->value;
             }
             return true;
@@ -159,35 +137,18 @@ bool MuxCommunication::check_nano(uint32_t value, uint16_t &mesureInt) {
     return false;
 }
 
-
-=======
->>>>>>> Stashed changes
-void MuxCommunication::update_crc(unsigned char &crc) {
-    for (int j = 0; j < 8; j++) {
+void MuxCommunication::update_crc(unsigned char &crc)
+{
+    for (int j = 0; j < 8; j++)
+    {
         if (crc & 1)
             crc ^= CRC7_POLY;
         crc >>= 1;
     }
 }
 
-<<<<<<< Updated upstream
-=======
-bool MuxCommunication::check_nano(uint32_t value, Leg_t &legId, LinearActuatorLeg_t &linear_actuator_id,
-                                  uint32_t &mesureInt) {
-    struct nanoValue *v = (struct nanoValue *) &value;
-    if (v->header == 0x55) {
-        if (v->check == get_crc(value & 0xFF, (value >> 8) & 0xFF)) {
-            legId = v->legId;
-            linear_actuator_id = v->actuatorId;
-            mesureInt = v->value;
-            return true;
-        }
-    }
-    return false;
-}
-
->>>>>>> Stashed changes
-unsigned char MuxCommunication::get_crc(unsigned char v1, unsigned char v2) {
+unsigned char MuxCommunication::get_crc(unsigned char v1, unsigned char v2)
+{
     unsigned char crc = 0;
     crc ^= v1;
     update_crc(crc);
@@ -195,14 +156,3 @@ unsigned char MuxCommunication::get_crc(unsigned char v1, unsigned char v2) {
     update_crc(crc);
     return crc;
 }
-
-<<<<<<< Updated upstream
-Callback<void()> MuxCommunication::main() {
-
-    while (true){
-        threadWorker();
-    }
-}
-
-=======
->>>>>>> Stashed changes
--- Mux/MuxCommunication.h
+++ Mux/MuxCommunication.h
@@ -1,27 +1,14 @@
 #ifndef MEGABOT_DRIVER_MUXCOMMUNICATION_H
 #define MEGABOT_DRIVER_MUXCOMMUNICATION_H
 
-<<<<<<< Updated upstream
-#include <MasterCommunication.h>
 #include "mbed.h"
 #include "Leg.h"
 #include "GlobalConfig.h"
-=======
-#include "mbed.h"
-#include "ComLinearActuator.h"
-
-
-#define BaudRate 115200
->>>>>>> Stashed changes
 
 const unsigned char CRC7_POLY = 0x91;
 
 class MuxCommunication {
-<<<<<<< Updated upstream
 public:
-=======
-    //FIXME classe de transfert pour les données
->>>>>>> Stashed changes
     /**
      * Initialisation des protocoles de communication pour recevoir l'élongation de chacun des LinearActurator.
      * Envoie ces valeurs à la classe ?
@@ -30,22 +17,17 @@ public:
      * @param channelB  : PinName       : pour le channel A du multiplexeur
      * @param TXLeg1    : PinName       : correspondant au TX de la Leg1 sélectionné par AB
      * @param TXLeg2    : PinName       : correspondant au TX de la Leg2 sélectionné par AB
-<<<<<<< Updated upstream
      * @param mainFlags : EventFlags    : pointeur vers le Flag d'arrêt d'urgence
      */
     MuxCommunication(PinName channelA, PinName channelB, PinName TX_legA, PinName TX_legB, EventFlags *mainFlags,
                      Leg *leg_A, Leg *leg_B);
 
+    void run();
+
     /**
      * @brief Comportement classique dans le Thread : attente d'un message soit disponible sur le Port sélectionner
      */
     void threadWorker();
-
-    /**
-     * @brief Equivalent du main pour le Thread associé à cette classe
-     * utile pour faire fonctionner de manière 'classique' le threadWorker et de pouvoir y ajouter des sécurités
-     */
-    Callback<void()> main();
 
 private:
     DigitalOut *A;
@@ -62,6 +44,7 @@ private:
 
     EventFlags *flag;
 
+    Thread MuxThread;
 
 // Communication tools
     uint32_t vA;
@@ -82,44 +65,6 @@ private:
     static unsigned char get_crc(unsigned char v1, unsigned char v2);
 
     bool check_nano(uint32_t value, uint16_t &mesureInt);
-=======
-     * @param emergency : EventFlags    : pointeur vers le Flag d'arrêt d'urgence
-     */
-    MuxCommunication(PinName channelA, PinName channelB, PinName TXLeg1, PinName TXLeg2, EventFlags* emergency, ComLinearActuator* interfaceLA);
-
-    /**
-     * Le Thread attend qu'un message soit disponible sur le Port sélectionner
-     */
-    void threadWorker();
-
-private:
-    DigitalOut* A;
-    DigitalOut* B;
-    BufferedSerial* TXLeg1;
-    BufferedSerial* TXLeg2;
-
-    uint8_t wordAB;
-    LinearActuatorLeg_t linearActuatorSelected;
-    Leg_t legSelected;
-
-    EventFlags* emergencyFlag;
-
-    ComLinearActuator* interfaceLA;
-
-
-// Communication tools
-    struct nanoValue{
-        uint32_t            value:10;
-        LinearActuatorLeg_t actuatorId:2;
-        Leg_t               legId:3;
-
-        uint32_t check:8;
-        uint32_t header:8;
-    };
-    static void update_crc(unsigned char &crc);
-    static unsigned char get_crc(unsigned char v1, unsigned char v2);
-    static bool check_nano(uint32_t value, Leg_t &legId, LinearActuatorLeg_t &linear_actuator_id, uint32_t &mesureInt);
->>>>>>> Stashed changes
 };
 
 
