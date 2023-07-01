85257e4692103f2adbd101d105026321d73d8353
leov66 <leo.v66.lov@gmail.com>
2023-07-01 17:54:08 +0200

    update file

--- Leg/Leg.cpp
+++ Leg/Leg.cpp
@@ -1,18 +1,12 @@
 #include "Leg.h"
 
-<<<<<<< Updated upstream
 Leg::Leg(LegId_t leg_id, LinearActuator *base_leg, LinearActuator *middle_leg, LinearActuator *end_leg) {
     legId = leg_id;
-=======
-Leg::Leg(Leg_t id_leg, LinearActuator *base_leg, LinearActuator *middle_leg, LinearActuator *end_leg) {
-
->>>>>>> Stashed changes
 
     linear_actuator_baseLeg = base_leg;
     linear_actuator_middleLeg = middle_leg;
     linear_actuator_endLeg = end_leg;
 }
-<<<<<<< Updated upstream
 
 void Leg::updateValuePositionInt(LinearActuatorId_t linear_actuator_select, uint16_t mesureInt) const {
     switch (linear_actuator_select) {
@@ -31,5 +25,3 @@ void Leg::updateValuePositionInt(LinearActuatorId_t linear_actuator_select, uint
 LegId_t Leg::getId() const {
     return legId;
 }
-=======
->>>>>>> Stashed changes
