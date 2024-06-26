70e6267895383d5b63db4e872e1e9e6d98809b12
Julien Allali <allali@enseirb-matmeca.fr>
2024-06-26 10:11:00 +0200

    sin.py is a sample demo code that use com and make a sin move on a actuator

--- python/sin.py
+++ python/sin.py
@@ -1,20 +1,54 @@
 import math
 import time
 import com
+import sys
 
-t=0
 
-c=com.ControlerHandler("sin.log")
+if len(sys.argv)!=6:
+    print(sys.argv[0]," leg actuator start_pos end_pos speed")
+    print("  leg: [1;4]")
+    print("  actuator: [1;3]")
+    print("  start_pos: [0.0,end_pos[")
+    print("  end_pos: ]start_pos;0.2]")
+    print("  speed: time in sec to make a cycle (full move from start_pos to end_pos to start_pos)")
+
+
+leg=int(sys.argv[1])
+actuator=int(sys.argv[2])
+start_pos=float(sys.argv[3])
+end_pos=float(sys.argv[4])
+speed=float(sys.argv[5])
+
+if start_pos<0 or start_pos>=end_pos or end_pos>0.2:
+    print("condition 0<=start_pos<end_pos<=0.2 not ok")
+    sys.exit(1)
+
+center=(start_pos+end_pos)/2.0
+magnitude=(end_pos-start_pos)/2.0
+
+
+
+
+print("initialize controler...")
+c=com.ControlerHandler("sin.log") 
+#c.print_string_msg(True)
 time.sleep(1)
-c.send_info(0.005)
+c.send_info(0.1) # ask for update of status from controler every 100ms
+t0=time.time()
+print("start move: use CRTL^C to stop")
 try:
     while True:
         t=time.time()
-        v=((math.sin(t/1.0)+1.0)/2.0) *0.1+0.05
-        print(v)
-        c.send_move(4,3,v)
-        time.sleep(0.01)
-        c.send_move(4,1,v,0.3)
+        v=math.sin((t-t0)/speed*2*math.pi)*magnitude + center
+#        print(t-t0,v)
+        
+#        c.send_move(1,1,v,1.0)
+#        c.send_move(2,1,v,1.0)      
+#        c.send_move(3,1,v,1.0)       
+#        c.send_move(4,1,v,1.0)
+#        time.sleep(0.05)
+#       
+        time.sleep(0.02)
 except:
     pass
 c.stop()
