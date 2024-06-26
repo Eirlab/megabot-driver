dc2f7dda8bbe7698ec8a3dc5b375f055032ce696
Julien Allali <allali@enseirb-matmeca.fr>
2024-06-26 09:27:56 +0200

    update com

--- python/com.py
+++ python/com.py
@@ -28,7 +28,7 @@ class ControlerHandler(Thread):
             if os.path.exists(pname)==False:
                 continue
             try:
-                port=Serial(port=pname, baudrate=115200, timeout=0.2)
+                port=Serial(port=pname, baudrate=2000000, timeout=0.01)
                 print('port ',pname,' opened')
             except Exception as e:
                 traceback.print_exc()
@@ -47,9 +47,9 @@ class ControlerHandler(Thread):
         self.start_time=time.time()
         self.legs=[]
         for a in [1,2,3,4]:
-            self.legs.append([{'enabled':False,'position':0,'target':0,'last_order':0,'time':0},
-                              {'enabled':False,'position':0,'target':0,'last_order':0,'time':0},
-                              {'enabled':False,'position':0,'target':0,'last_order':0,'time':0}])
+            self.legs.append([{'enabled':False,'position':0,'target':0,'pwm':0,'pwmtarget':0,'status':-1,'last_order':0,'time':0},
+                              {'enabled':False,'position':0,'target':0,'pwm':0,'pwmtarget':0,'status':-1,'last_order':0,'time':0},
+                              {'enabled':False,'position':0,'target':0,'pwm':0,'pwmtarget':0,'status':-1,'last_order':0,'time':0}])
         self.start()
     def stop(self):
         self.shouldStop=True
@@ -62,6 +62,13 @@ class ControlerHandler(Thread):
         """
         o=com_data.Order()
         o.move(leg,actuator,position,pwm)
+        data=o.pack()
+        for s in self.serials:
+            s.write(data)
+
+    def send_pid(self,leg,actuator,kp,ki,kd):
+        o=com_data.Order()
+        o.pid(leg,actuator,kp,ki,kd)
         data=o.pack()
         for s in self.serials:
             s.write(data)
@@ -82,20 +89,39 @@ class ControlerHandler(Thread):
 
     def print_string_msg(self,b):
         self.print_str_msg=b
+    def startlog(self,filename):
+        if self.logfile!=None:
+            self.logfile.close()
+            self.logfile=None
+        if filename!=None:
+            self.logfile=open(filename,'w')
+    def print_status(self):
+        for l in range(4):
+            for a in range(3):
+                if self.legs[l][a]['status']!=255:
+                    print('L',l+1,a+1,'[status=',self.legs[l][a]['status'],
+                          ',pos=',self.legs[l][a]['position'],
+                          ',target=',self.legs[l][a]['target'],
+                          ',pwm=',self.legs[l][a]['pwm'],
+                          ',pwmtarget=',self.legs[l][a]['pwmtarget'],']')
     
     def run(self):
+        rawfile=open("raw.log","wb")
         while self.shouldStop==False:
             action=False
             for s in self.serials:
                 if s.in_waiting:
                     action=True
-                    d=s.read(500)
+                    d=s.read(5000)
+                    if len(d)>0:
+                        rawfile.write(d)
+                    #print("read ",len(d)," bytes")
                     p=0
                     while p<len(d):
                         if d[p]==0xA5 and (len(d)-p)>=com_data.Infos.size:
                             i=com_data.Infos(d[p:])
                             if i.check():
-                                print(".",end='')
+                                #print(".",end='')
                                 for a in range(3):
                                     self.legs[i.leg-1][a]['enabled']=True
                                     self.legs[i.leg-1][a]['position']=i.actuators[a].position/10000.0
@@ -105,39 +131,47 @@ class ControlerHandler(Thread):
                                     self.legs[i.leg-1][a]['status']=i.actuators[a].status
                                     self.legs[i.leg-1][a]['time']=time.time()-self.start_time
                                     if self.logfile!=None:
-                                        self.logfile.write("%d;%d;%d;%.3f;%.3f;%.3f;%.3f;%d;%.3f\n"%(i.leg,a+1,self.legs[i.leg-1][a]['status'],
+                                        try:
+                                            self.logfile.write("%d;%d;%d;%.5f;%.5f;%.5f;%.5f;%.5f\n"%(i.leg,a+1,self.legs[i.leg-1][a]['status'],
                                                                         self.legs[i.leg-1][a]['position'],
                                                                         self.legs[i.leg-1][a]['target'],
                                                                         self.legs[i.leg-1][a]['pwm'],
                                                                         self.legs[i.leg-1][a]['pwmtarget'],
-                                                                        self.legs[i.leg-1][a]['status'],
                                                                         self.legs[i.leg-1][a]['time']))
+                                        except:
+                                            pass
+                                #print("receive infos...:")
+                                #self.print_status()
                                 p+=com_data.Infos.size                                
                             else:
-                                #print("infos check wrong:",i)
+                                print("infos check wrong:",i,"\np=",p,"\nlen(d)=",len(d),"\n\n")
                                 p+=1
-                        elif d[p]==0x53 and (len(d)-p)>=com_data.GlobalInfos.size:
+                        elif d[p]==com_data.GlobalInfos.headerValue() and (len(d)-p)>=com_data.GlobalInfos.size:
                             i=com_data.GlobalInfos(d[p:])
                             if i.check():
-                                #print("stats: main:",end='')
-                                #print(i.main_ticking,end='')
-                                #for l in range(4):
-                                #    for a in range(3):
-                                #        print('L',l+1,a+1,'[',i.actuator_ticking[l*3+a],',',i.read_ticking[l*3+a],']',end='')
-                                #print()
+                                print("stats: main:",end='')
+                                print(i.main_ticking,end='')
+                                for l in range(4):
+                                    for a in range(3):
+                                        print('L',l+1,a+1,'[',i.actuator_ticking[l*3+a],',',i.read_ticking[l*3+a],']',end='')
+                                print()
                                 p+=com_data.GlobalInfos.size
                             else:
-                                #print("main stats check wrong:",i)
+                                print("\n\nmain stats check wrong:",i,"\np=",p,"\nlen(d)=",len(d),"\n",d[p:],"\n\n")
                                 p+=1
                         else:
                             if self.print_str_msg:
-                                if chr(d[p]).isprintable() or chr(d[p])=='\n':
-                                    print(chr(d[p]),end='')
+                                x=chr(d[p])
+                                if x in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,-;#()=+*0123456789 .\n':
+                                    print(x,end='')
+                                #else:
+                                    #print("\n\n",p,"/",len(d)," %x "%(ord(x)),end='')
                             p+=1
             if action==False:
-                time.sleep(0.001)
+                time.sleep(0.0001)
         if self.logfile!=None:
             self.logfile.close()
+        rawfile.close()
         print("controler thread leaving")
         
 
@@ -145,11 +179,65 @@ class ControlerHandler(Thread):
 if __name__ == "__main__":
     try:
         import sys
-        c=ControlerHandler(sys.argv[1])
+        from math import pi, sin
+        import readline
+        import atexit
+        if len(sys.argv)<2:
+            c=ControlerHandler()
+        else:
+            c=ControlerHandler(sys.argv[1])
+        ## readline stuff
+        histfile = ".history"
+        try:    
+            readline.read_history_file(histfile)
+            # default history len is -1 (infinite), which may grow unruly
+            readline.set_history_length(1000)
+        except FileNotFoundError:
+            pass
+        atexit.register(readline.write_history_file, histfile)
+        ## end readline stuff
         c.print_string_msg(True)
         time.sleep(0.2)
-        c.send_info(0.0001)
-        input("waiting...")
+        c.send_info(0.01)
+        order=""
+        while order!="q":
+            cmd=order.split()
+            try:
+                if len(cmd)==0:
+                    pass
+                elif cmd[0]=='s':
+                    c.send_stop()
+                elif cmd[0]=='l':
+                    c.startlog(cmd[1])
+                elif cmd[0]=='i':
+                    c.send_info(float(cmd[1]))
+                elif cmd[0]=='p':
+                    c.send_pid(int(cmd[1]),int(cmd[2]),float(cmd[3]),float(cmd[4]),float(cmd[5]))
+                elif cmd[0]=='m':
+                    c.send_move(int(cmd[1]),int(cmd[2]),float(cmd[3]),float(cmd[4]))
+                elif cmd[0]=='sin':
+                    leg=int(cmd[1])
+                    actu=int(cmd[2])
+                    period=float(cmd[3])  # in s
+                    maxp=float(cmd[4]) # max pwm
+                    print("send actuator to 0.1...")
+                    c.send_move(leg,actu,0.1,0.3)
+                    time.sleep(3)
+                    t=time.time()
+                    dt=0
+                    c.startlog("sinus.data")
+                    while dt<2*period:
+                        p=(sin(dt/period*2*pi)+1.0)/2.0 *0.1 + 0.05
+                        c.send_move(leg,actu,p,maxp)
+                        time.sleep(0.002)
+                        dt=time.time()-t
+                    c.startlog(None)
+            except Exception as e:
+                print("cmd exception:",e, "cmd:",cmd)
+                pass
+            order=input("? ")
+            order=order.strip()
+        c.send_stop()
     except Exception as e:
         print("Exception:",e)
         pass
--- python/com_data.py
+++ python/com_data.py
@@ -21,35 +21,49 @@ class Order(cstruct.MemCStruct):
     __byte_order__ = cstruct.LITTLE_ENDIAN
     
     __def__ = """
-        #define ORDER_HEADER 0x52
+        #define ORDER_HEADER 0x13
         #define MOVE 1
         #define STOP 2
         #define PRINT 4
+        #define PID 8
 
         struct {
             unsigned char head;
             unsigned char order;
             unsigned char leg;
             unsigned char actuator;
-            unsigned short value;
-            unsigned short pwm;
+            unsigned short v1;
+            unsigned short v2;
+            unsigned short v3;
             unsigned char crc1;
             unsigned char crc2;
         }
     """
+    def headerValue():
+        return cstruct.getdef('ORDER_HEADER')
     def update_crc(self):
         data=self.pack()
         self.crc1=get_crc(data[:-2])
         data=self.pack()
         self.crc2=get_crc(data[:-1])
         
+    def pid(self,leg,actuator,kp,ki,kd):
+        self.head=cstruct.getdef('ORDER_HEADER')
+        self.order=cstruct.getdef('PID')
+        self.leg=leg
+        self.actuator=actuator
+        self.v1 = int(kp * 100)
+        self.v2 = int(ki * 100)
+        self.v3 = int(kd * 100)
+        self.update_crc()
     def move(self,leg,actuator,position,pwm=1.0):
         self.head=cstruct.getdef('ORDER_HEADER')
         self.order=cstruct.getdef('MOVE')
         self.leg=leg
         self.actuator=actuator
-        self.value = int(position * 10000)
-        self.pwm=int(pwm*10000)
+        self.v1 = int(position * 10000)
+        self.v2=int(pwm*10000)
+        self.v3=0
         self.update_crc()
     def stop(self):
         self.head=cstruct.getdef('ORDER_HEADER')
@@ -58,7 +72,7 @@ class Order(cstruct.MemCStruct):
     def print(self,freq):
         self.head=cstruct.getdef('ORDER_HEADER')
         self.order=cstruct.getdef('PRINT')
-        self.value = int(freq*10000)
+        self.v1 = int(freq*10000)
         self.update_crc()
         
 
@@ -83,7 +97,8 @@ class Infos(cstruct.MemCStruct):
            unsigned char crc2;
         };
     """
-
+    def headerValue():
+        return cstruct.getdef('INFO_HEADER')
     def check(self):
         try:
             d=self.pack()
@@ -104,7 +119,7 @@ class GlobalInfos(cstruct.MemCStruct):
     __byte_order__ = cstruct.LITTLE_ENDIAN
     
     __def__ = """
-    #define GLOBAL_INFO_HEADER 0x53
+    #define GLOBAL_INFO_HEADER 0xD1
 
     struct GlobalInfo {
       unsigned char head;
@@ -117,6 +132,8 @@ class GlobalInfos(cstruct.MemCStruct):
       unsigned char crc2;
     };
     """
+    def headerValue():
+        return cstruct.getdef('GLOBAL_INFO_HEADER')
     def check(self):
         try:
             d=self.pack()
